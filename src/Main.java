715b81f63c5da9b3dcf3054ddddd9edbe488c951
Zhong Tao <ztao084@uottawa.ca>
2023-11-04 18:53:52 -0400

    new changes

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-
-
 public class Main {
 
     public static void main(String[] args) {
+        // Check if a filename has been provided as an argument
+        if (args.length < 1) {
+            System.out.println("Usage: java Main <filename>");
+            return; // Exit if no filename is provided
+        }
+
+        // Use the provided filename from the command line
+        String filename = args[0];
 
         LexicalAnalyzer lex = new LexicalAnalyzer();
 
-        lex.readFile("input20.txt");
+        // Pass the filename to the readFile method
+        lex.readFile(filename);
 
         SyntaxAnalyzer syn = new SyntaxAnalyzer(lex.gettokenStack());
 
-        System.out.println(syn.analyze());
+        // Output the result of the analysis
+        if(filename!=""){System.out.println(syn.analyze());}
     }
 }
